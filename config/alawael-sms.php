d34fe97948ba284638f7be2447517ad61a865c89
zayedadel <zayedadel95@gmail.com>
2025-01-16 06:44:03 +0300

    chor: add balance url env var

--- config/alawael-sms.php
+++ config/alawael-sms.php
@@ -5,6 +5,6 @@ return [
     'username' => env('ALAWAEL_USERNAME'),
     'password' => env('ALAWAEL_PASSWORD'),
     'send_url' => env('ALAWAEL_URL', "https://sms.alawaeltec.com/MainServlet"),
-    'balance_url' => 'http://185.216.203.97:8070/AlawaelEstalam',
+    'balance_url' => env('ALAWAEL_BALANCE_URL', "http://185.216.203.97:8070/AlawaelEstalam"),
     'coding' => env('ALAWAEL_CODING', 2),
 ];
